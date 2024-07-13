028749d1a0751be39ed679ddfd5923121d2ffc50
ZestyZeeshan <mohdzeeshanmohammad@gmail.com>
2024-07-13 23:34:39 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,19 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+
+// var add = function(a,b){
+//     return a+b;
+// }
+
+
+// var add = (a,b) =>{
+//     return a+b;
+// }
+
+
+var add = (a,b) => a+b;
+var ans = add(1,8)
+
+console.log(ans);
