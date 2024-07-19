25d7615d03bce6ec04168c7b289ee1badcee49db
ZestyZeeshan <mohdzeeshanmohammad@gmail.com>
2024-07-19 19:57:31 +0530

    Authentecation and Authorisation

--- Server.js
+++ Server.js
@@ -37,15 +37,54 @@
 // console.log('result is now'+ result);
 
  
-const express = require('express')
+const express = require('express');
 const app = express();
-const db = require('./db')
+const db = require('./db');
 require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./models/Person');
+
 
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json()); //req.body
 const PORT = process.env.PORT || 3000;
 
+
+
+//Middleware Function
+const logRequest = (req,res,next)=>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to:${req.originalUrl}`);
+    next();
+}
+
+app.use(logRequest);
+
+passport.use(new LocalStrategy(async (username, password,done)=>{
+    try{
+        console.log('Received credentials:' , username, password);
+        //username ko database wale username se match krenge
+        const user = await Person.findOne({username});
+        if(!user)
+            return done(null, false, {message : 'Incorrect username.'});
+
+        const isPsswordMatch = await user.comparePassword(password);
+
+        if(isPsswordMatch){
+            return done(null,user);
+        }else{
+            return done(null, false,{message: 'Incorrect password'});
+        }
+
+    }catch(err){
+        return done(err);
+    }
+}));
+
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local' , {session : false})
 
 app.get('/', function(req,res){
     res.send('helloo welcom! how can i help u')
@@ -101,7 +140,7 @@ app.get('/', function(req,res){
 const personRoutes = require('./routes/personroutes');
 
 //use the router
-app.use('/person', personRoutes);
+app.use('/person',localAuthMiddleware, personRoutes);
 
 //import the menu router
 const menuRouter = require ('./routes/menuRoutes')
--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -29,7 +29,7 @@ router.get('/',async (req,res)=>{
 
     }catch(err){
         console.log('Error:', err);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Internal server error' })
     }
 })
 
